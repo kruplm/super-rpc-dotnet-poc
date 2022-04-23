87ea7820dbfdfa03fad27fe7fb6bfbb6a231064c
Sly1024 <szilveszter.safar@gmail.com>
2022-04-23 11:46:32 +0000

    Tests update 1

--- super-rpc.tests/SuperRpcTests.cs
+++ super-rpc.tests/SuperRpcTests.cs
@@ -75,4 +75,81 @@ public class SuperRpcTests
 
         return taskSource.Task;
     }
+
+    public class HostObjectTests : SuperRpcTests
+    {
+        interface IHostObject {
+            int SyncFunc(int a, int b);
+            void FailSyncFunc();
+            Task<string> AsyncFunc(string ping);
+            Task<string> FailAsyncFunc(string ping);
+            string roID { get; }
+            int Counter { get; set; }
+
+            event EventHandler<EventArgs> CounterChanged;
+        }
+
+        public class HostObject : IHostObject
+        {
+            public string roID => "readonly";
+
+            private int counter = 0;
+
+            public int Counter { 
+                get => counter; 
+                set {
+                    counter++;
+                    CounterChanged?.Invoke(this, new EventArgs());
+                }
+            }
+
+            public event EventHandler<EventArgs>? CounterChanged;
+
+            public async Task<string> AsyncFunc(string ping)
+            {
+                await Task.Delay(1000);
+                return ping + " pong";
+            }
+
+            public async Task<string> FailAsyncFunc(string ping)
+            {
+                await Task.Delay(1000);
+                throw new InvalidOperationException(ping + "ooops");
+            }
+
+            public void FailSyncFunc()
+            {
+                throw new InvalidOperationException("No");
+            }
+
+            public int SyncFunc(int a, int b)
+            {
+                return a + b;
+            }
+            
+        }
+
+        HostObject hostObj;
+
+        public HostObjectTests()
+        {
+            hostObj = new HostObject();
+
+            rpc1.RegisterHostObject("service12", hostObj, new ObjectDescriptor {
+                ReadonlyProperties = new [] { "roID" },
+                ProxiedProperties = new PropertyDescriptor[] { "Counter" },
+                Functions = new [] {
+                    new FunctionDescriptor { Name = "syncFunc", Returns = FunctionReturnBehavior.Sync },
+                    new FunctionDescriptor { Name = "failSyncFunc", Returns = FunctionReturnBehavior.Sync },
+                    new FunctionDescriptor { Name = "asyncFunc", Returns = FunctionReturnBehavior.Async },
+                    new FunctionDescriptor { Name = "failAsyncFunc", Returns = FunctionReturnBehavior.Async },
+                }
+            });
+        }
+
+        [Fact]
+        void SyncFuncSuccess() {
+
+        }
+    }
 }
